9e5b9e1d542b2b36a3105890f3ee9032c56e8091
Jakub Świadek <jakubswiadek@me.com>
2016-09-02 22:43:19 +0300

    Added more descriptions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,7 +3,7 @@
 
 $(document).scroll(function() {
     var cutoff = $(window).scrollTop();
-    
+
     $('.location').each(function(){
         if($(this).offset().top + $(this).height() > cutoff){
 		document.getElementById("point").setAttribute("cx", $(this).attr('position-x'))
@@ -18,19 +18,18 @@ $(function() {
     	effect : "fadeIn"
 	});
 });
+// 
+// $(document).ready(function () {
+//     slider();
+// });
+//
+// $(window).scroll(function () {
+//     slider();
+// });
 
-$(document).ready(function () {
-    slider();
-});
-
-$(window).scroll(function () {
-    slider();
-});
-
-function slider() {
-    if (document.body.scrollTop > 600)
-        $('#map').stop().animate({"margin-left": '0'});
-    else
-        $('#map').stop().animate({"margin-left": '-200'});
-}
-
+// function slider() {
+//     if (document.body.scrollTop > 600)
+//         $('#map').stop().animate({"margin-left": '0'});
+//     else
+//         $('#map').stop().animate({"margin-left": '-200'});
+// }
